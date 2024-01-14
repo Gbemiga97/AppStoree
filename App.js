c6c0efa49fe345d2db218ebee81bd42845763630
gbemiga97 <holuwagbemiga97@gmail.com>
2024-01-14 12:03:33 +0100

    third commit

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+// App.js
+
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+
+
 
 export default function App() {
   return (
